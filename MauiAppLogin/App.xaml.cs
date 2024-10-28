a548a1099a5b2f87c0b5daf4cbfc40e81dd4a6a9
marcelogalerani <marcelo.galerani@etec.sp.gov.br>
2024-10-28 17:25:20 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new AppShell();
         }
-    }
-}
+
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 700;
+
+            return window;
+        }
+
+    } //Fecha classe
+ } //Fecha namespace
+
